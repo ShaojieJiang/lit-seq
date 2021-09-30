aa6d3ffcd5413c41e3ef8a6589f6aa1ef2590ee0
Shaojie Jiang <sjiang@ilps-h1.science.uva.nl>
2021-09-30 13:54:53 +0200

    Using callbacks in the Hydra way.

--- lightning_transformers/cli/train.py
+++ lightning_transformers/cli/train.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from lightning_transformers.core.callback import ProgressBarsWithSteps
 from typing import Any, Optional
 
 import hydra
@@ -56,9 +55,6 @@ def run(
     trainer = instantiator.trainer(
         trainer,
         logger=logger,
-        callbacks=[
-            ProgressBarsWithSteps(trainer['progress_bar_refresh_rate'], trainer['process_position'])
-        ]
     )
 
     trainer.fit(model, datamodule=data_module)
