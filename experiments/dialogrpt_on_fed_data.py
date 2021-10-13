bd40364d4c1b379ea2c93bfa0321e9c7af822e25
Shaojie Jiang <sjiang@ilps-h1.science.uva.nl>
2021-10-13 18:22:59 +0200

    Added experiment script for calculating DialogRPT scores and FED human correlations.

--- experiments/dialogrpt_on_fed_data.py
+++ experiments/dialogrpt_on_fed_data.py
@@ -2,10 +2,14 @@ import json
 
 import numpy as np
 import requests
+import torch
+import tqdm
 from scipy.stats.mstats_basic import pearsonr
 from scipy.stats.stats import spearmanr
-import tqdm
-from experiments import fed_metric
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+from experiments.dialog_rpt_scorer import DialogRPTScorer
+
 
 # Evaluate
 # conversation = "<|endoftext|> Hi! <|endoftext|> Hello, how is your day? <|endoftext|> It's good. It's raining a bit, but I am enjoying a good book. How about you? <|endoftext|> It's good, I just got back from walking my dog What book did you read?"
@@ -13,22 +17,28 @@ _URL = "http://shikib.com/fed_data.json"
 
 r = requests.get(_URL)
 
-model, tokenizer = fed_metric.load_models("microsoft/DialoGPT-large")
+model = DialogRPTScorer(80, fp16=False).cuda()
+
+@torch.no_grad()
+def score(text):
+    score = model.score_text(text)
+    return score.item()
 
 data = json.loads(r.content)
 model_scores = []
 human_scores = []
 for row in tqdm.tqdm(data):
     if 'response' in row: # this is a turn annotation, add it to dict
-        text = f"<|endoftext|> {row['context']}\n{row['response']}".replace('User: ', '').replace('System: ', '').replace('\n', ' <|endoftext|> ')
+        text = f"{row['context']}\n{row['response']}".replace('User: ', '').replace('System: ', '')
+        history = text.split('\n')[-2:]
+        text = ' <|end_of_text|> '.join(history)
+        # text = text.replace('\n', ' <|endoftext|> ') # using full history results in poor correlations
         engaging = row['annotations']['Engaging']
         avg_engaging = np.mean(engaging)
         human_scores.append(avg_engaging)
         
-        scores = fed_metric.evaluate(text,
-                            model,
-                            tokenizer)
-        model_scores.append(scores['engaging'])
+        engaging = score(text)
+        model_scores.append(engaging)
 
 spearman = spearmanr(model_scores, human_scores)
 pearson = pearsonr(model_scores, human_scores)
